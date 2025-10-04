630f0b658e98bd8f753e48ade2b62bfecf38732f
Korya25 <????? ??? GitHub>
2025-10-04 18:26:23 +0300

    edit transtions

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -18,75 +18,81 @@ class AppRouter {
         ? AppRoutes.unessentialSplash
         : AppRoutes.unessentialSplash,
     routes: [
+      // Splash
       GoRoute(
         path: AppRoutes.splash,
         name: AppRoutes.splash,
-        pageBuilder: (context, state) => AppTransitions.noTransition(
+        pageBuilder: (context, state) => AppTransitions.fade(
           context: context,
           state: state,
           child: const SplashView(),
         ),
       ),
+      // Unessential Splash
       GoRoute(
         path: AppRoutes.unessentialSplash,
         name: AppRoutes.unessentialSplash,
-        pageBuilder: (context, state) => AppTransitions.noTransition(
+        pageBuilder: (context, state) => AppTransitions.fade(
           context: context,
           state: state,
           child: const UnessentialSplashView(),
         ),
       ),
+      // Onboarding
       GoRoute(
         path: AppRoutes.onboarding,
         name: AppRoutes.onboarding,
-        pageBuilder: (context, state) => AppTransitions.noTransition(
+        pageBuilder: (context, state) => AppTransitions.slideFromRight(
           context: context,
           state: state,
           child: const OnboardingView(),
         ),
       ),
+      // Auth Gate
       GoRoute(
         path: AppRoutes.authGate,
         name: AppRoutes.authGate,
-        pageBuilder: (context, state) => AppTransitions.noTransition(
+        pageBuilder: (context, state) => AppTransitions.fadeScale(
           context: context,
           state: state,
           child: const AuthGate(),
         ),
       ),
-
+      // Login
       GoRoute(
         path: AppRoutes.login,
         name: AppRoutes.login,
-        pageBuilder: (context, state) => AppTransitions.noTransition(
+        pageBuilder: (context, state) => AppTransitions.slideFromRight(
           context: context,
           state: state,
           child: const LoginView(),
         ),
       ),
+      // Signup
       GoRoute(
         path: AppRoutes.signUp,
         name: AppRoutes.signUp,
-        pageBuilder: (context, state) => AppTransitions.noTransition(
+        pageBuilder: (context, state) => AppTransitions.slideFromRight(
           context: context,
           state: state,
           child: const SignupView(),
         ),
       ),
+      // Forgot Password
       GoRoute(
         path: AppRoutes.forgotPass,
         name: AppRoutes.forgotPass,
-        pageBuilder: (context, state) => AppTransitions.noTransition(
+        pageBuilder: (context, state) => AppTransitions.slideFromRight(
           context: context,
           state: state,
           child: const ForgotPasswordView(),
         ),
       ),
-
+      // Home
       GoRoute(
         path: AppRoutes.home,
         name: AppRoutes.home,
-        pageBuilder: (context, state) => AppTransitions.noTransition(
+        pageBuilder: (context, state) => AppTransitions.slideAndFade(
           context: context,
           state: state,
           child: const HomeView(),
